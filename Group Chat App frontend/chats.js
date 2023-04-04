05f4806a8a90759aab6d218ab58322b7af5db656
Atharva Vibhute <94898067+atharvavibe@users.noreply.github.com>
2023-04-04 15:40:54 +0530

    Add files via upload

--- Group Chat App frontend/chats.js
+++ Group Chat App frontend/chats.js
@@ -1,21 +1,29 @@
 const outgoingMessage = document.getElementById('outgoing message')
 
-window.addEventListener('DOMContentLoaded', () => {
-    const token = localStorage.getItem('token')
-    const response = axios.get('http://localhost:3000/chat/getmessage', {headers: {"Authorization": token}}).then(response => {
-        console.log(response)
-    })
-    
+window.addEventListener('DOMContentLoaded', showUserMessages())
 
-})
+async function showUserMessages(){
+    setInterval(() => {
+        getChats()
+    }, 1000)
+}
 
-
+async function getChats(){
+   try{ 
+        const token = localStorage.getItem('token')
+        const response = axios.get('http://localhost:3000/chat/getmessage', {headers: {"Authorization": token}}).then(response => {
+            console.log(response)
+        })
+    }catch(err){
+        console.log(err)
+    }
+}
 
 async function sendChat(e){
     try{
         e.preventDefault()
         const userChat = e.target.chat.value
-        showMessageOnUI(userChat)
+        //showMessageOnUI(userChat)
         if(!userChat){
             return alert('Enter a message!')
         }
